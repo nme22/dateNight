4255e4f122449a115ea693a40e3f8f8884c40e21
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-10-08 09:24:06 -0400

    last save before some sleep

--- datingapp/pages/FormDate.js
+++ datingapp/pages/FormDate.js
@@ -3,35 +3,44 @@ import styles from '../styles/Form.module.css'
 
 const FormDate = () => {
     return ( 
-        <div >
+        <div className="form">
             <h1> Create Your Date! </h1>
-            <form className="form-date">
+            <form >
                 
 
                 <label>Who:</label>
-                <input id="name" type="text" autoComplete="name" required />
+                <p className="form-field"><input id="name" type="text" autoComplete="name" required  placeholder="Who are you going with?"/></p>
                 
+                <label>Phone number:</label>
+                <p className="form-field"><input id="when" type="number"  required placeholder="Phone Number"/></p>
                 
                 <label>What:</label>
-                <input  id="what" type="text" autoComplete="name" required />
+                <p className="form-field"><input  id="what" type="text"  required placeholder="What are we doing?"/></p>
                 
 
                 <label>Where:</label>
-                <input id="where" type="text" autoComplete="name" required />
+                <p className="form-field"><input id="where" type="text"  required placeholder="Where we going?"/></p>
                 
                 
                 <label>When:</label>
-                <input id="when" type="text" autoComplete="name" required />
+                <p className="form-field"><input id="when" type="text"  required placeholder="When is this?"/></p>
                 
-
-                <label>Notes:</label>
-                <input id="note" type="text" autoComplete="note" required />
+                <label>Go again?:</label>
+                <select id="repeat" type="boolean"  required placeholder="Do this date again?">
+                    <option value="Yes">Yes</option>
+                    <option value="No">No Shot</option>
+                </select>
 
                 
             </form>
 
+            <label>Notes:</label>
+                <textarea className="form-field-textarea" id="note" type="text" placeholder="Noteable things that happened during the date"></textarea>
+
             <Link href="/Previous Dates">
-                    <button className={styles.btn} type="submit">Create Your Date!</button>
+                    <button className={styles.btn} >
+                        <h3>Create Your Date!</h3>
+                    </button>
             </Link>
         </div>
      );
