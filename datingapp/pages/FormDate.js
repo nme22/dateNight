39b1d637e1fa7a491449735f594159f478b51001
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-10-19 19:44:33 -0400

    adding some chakra to the date form

--- datingapp/pages/FormDate.js
+++ datingapp/pages/FormDate.js
@@ -1,88 +1,74 @@
 import Link from 'next/link';
 import styles from '../styles/Form.module.css';
+import {
+  FormControl,
+  FormLabel,
+  Text,
+  Input,
+  VStack,
+  Heading,
+} from '@chakra-ui/react';
 
 const FormDate = () => {
   return (
-    <div className="form">
-      <h1 className="center"> Create Your Date! </h1>
-      <form>
-        <label>Who:</label>
-        <p className="form-field">
-          <input
-            id="dateName"
+    <VStack>
+      <div className="form">
+        <Heading alignSelf="center"> Create Your Date! </Heading>
+        <FormControl colSpan={2}>
+          <Text>Who are you going with:</Text>
+          <Input
             type="text"
             autoComplete="name"
             required
-            placeholder="Who are you going with?"
+            placeholder="Name of person invited"
           />
-        </p>
 
-        <label>Phone number:</label>
-        <p className="form-field">
-          <input
-            id="datePhone"
-            type="number"
+          <Text>Contact Number:</Text>
+          <Input type="text" required placeholder="Name of person invited" />
+
+          <Text>What are we doing?</Text>
+          <Input type="text" required placeholder="What we're doing" />
+
+          <Text>Where we going?</Text>
+          <Input type="text" required placeholder="Location" />
+
+          <label>When:</label>
+          <p className="form-field">
+            <input
+              id="dateWhen"
+              type="text"
+              required
+              placeholder="When is this?"
+            />
+          </p>
+
+          <label>Go again?:</label>
+          <select
+            id="repeat"
+            type="boolean"
             required
-            placeholder="Phone Number"
-          />
-        </p>
+            placeholder="Do this date again?"
+          >
+            <option value="Yes">Yes</option>
+            <option value="No">No Shot</option>
+          </select>
+        </FormControl>
 
-        <label>What:</label>
-        <p className="form-field">
-          <input
-            id="dateWhat"
-            type="text"
-            required
-            placeholder="What are we doing?"
-          />
-        </p>
+        <label>Notes:</label>
+        <textarea
+          className="form-field-textarea"
+          id="note"
+          type="text"
+          placeholder="Noteable things that happened during the date"
+        ></textarea>
 
-        <label>Where:</label>
-        <p className="form-field">
-          <input
-            id="dateWhere"
-            type="text"
-            required
-            placeholder="Where we going?"
-          />
-        </p>
-
-        <label>When:</label>
-        <p className="form-field">
-          <input
-            id="dateWhen"
-            type="text"
-            required
-            placeholder="When is this?"
-          />
-        </p>
-
-        <label>Go again?:</label>
-        <select
-          id="repeat"
-          type="boolean"
-          required
-          placeholder="Do this date again?"
-        >
-          <option value="Yes">Yes</option>
-          <option value="No">No Shot</option>
-        </select>
-      </form>
-
-      <label>Notes:</label>
-      <textarea
-        className="form-field-textarea"
-        id="note"
-        type="text"
-        placeholder="Noteable things that happened during the date"
-      ></textarea>
-
-      <Link href="/Previous Dates">
-        <button className={styles.btn}>
-          <h3>Create Your Date!</h3>
-        </button>
-      </Link>
-    </div>
+        <Link href="/Previous Dates">
+          <button className={styles.btn}>
+            <h3>Create Your Date!</h3>
+          </button>
+        </Link>
+      </div>
+    </VStack>
   );
 };
 
--- datingapp/pages/_app.js
+++ datingapp/pages/_app.js
@@ -1,12 +1,15 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+import '../styles/globals.css';
+import Layout from '../components/Layout';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
