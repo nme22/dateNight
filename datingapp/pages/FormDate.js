2a8ab57b5b55d2b8bf3dd04ed62c16837a0f57a0
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-10-20 02:47:27 -0400

    save for the night

--- datingapp/pages/FormDate.js
+++ datingapp/pages/FormDate.js
@@ -1,24 +1,27 @@
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
 import Link from 'next/link';
 import styles from '../styles/Form.module.css';
 import {
+  Button,
+  Divider,
   FormControl,
   FormLabel,
   Input,
   VStack,
   Heading,
   Textarea,
+  Select,
 } from '@chakra-ui/react';
 
 const FormDate = () => {
   return (
-    <VStack>
-      <div className="form">
-        <Heading alignSelf="center"> Create Your Date! </Heading>
-        <FormControl colSpan={2}>
+    <VStack py={2}>
+      <Heading size="2xl"> Create Your Date! </Heading>
+      <Divider />
+      <div>
+        <FormControl>
           <FormLabel>Who are you going with:</FormLabel>
           <Input
+            focusBorderColor="Pink"
             type="text"
             autoComplete="name"
             required
@@ -26,44 +29,65 @@ const FormDate = () => {
           />
 
           <FormLabel>Contact Number:</FormLabel>
-          <Input type="text" required placeholder="Name of person invited" />
+          <Input
+            focusBorderColor="Pink"
+            type="text"
+            required
+            placeholder="Name of person invited"
+          />
 
           <FormLabel>What we're doing:</FormLabel>
-          <Input type="text" required placeholder="What we're doing" />
+          <Input
+            focusBorderColor="Pink"
+            type="text"
+            required
+            placeholder="What we're doing"
+          />
 
           <FormLabel>Where we're going:</FormLabel>
-          <Input type="text" required placeholder="Location" />
+          <Input
+            focusBorderColor="Pink"
+            type="text"
+            required
+            placeholder="Location"
+          />
 
           <FormLabel>When we're doing this:</FormLabel>
-          <Input type="datetime-local" />
-          {/* <DatePicker
-            selected={''}
-            type="datetime-local"
-            onChange={(date) => setStartDate(date)}
-          /> */}
+          <Input focusBorderColor="Pink" type="datetime-local" />
 
           <FormLabel>Would you go on this date again?</FormLabel>
-          <select
-            id="repeat"
+          <Select
+            focusBorderColor="Pink"
             type="boolean"
             required
-            placeholder="Do this date again?"
+            placeholder="Again?"
+            textAlign="center"
           >
             <option value="Yes">Yes</option>
             <option value="No">No Shot</option>
-          </select>
+          </Select>
         </FormControl>
 
         <FormLabel>Notes:</FormLabel>
         <Textarea
+          focusBorderColor="Pink"
           type="text"
           placeholder="Noteable things that happened during the date"
         ></Textarea>
 
         <Link href="/Previous Dates">
-          <button className={styles.btn}>
-            <h3>Create Your Date!</h3>
-          </button>
+          <Button
+            display="flex"
+            w={150}
+            p={2}
+            m={2}
+            bg="palevioletred"
+            color="white"
+            textAlign="right"
+            borderRadius={4}
+          >
+            Create Your Date!
+          </Button>
         </Link>
       </div>
     </VStack>
